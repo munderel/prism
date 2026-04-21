14f6d04c719edb0b01b3b72b6f4ace737a676c50
munderel <munder.elgummi@gmail.com>
2026-04-21 14:23:38 -0400

    polish: google event color fidelity + task/workblock status styling
    - CalendarSplitView: use FullCalendar backgroundColor for Google events;
      dim+strikethrough DONE/DROPPED tasks; visually distinguish workblock
      COMPLETED/MISSED/PARTIAL outcomes
    - PowerDownRitual: pass mode='schedule_tasks' to CalendarSplitView

--- src/components/powerdown/PowerDownRitual.tsx
+++ src/components/powerdown/PowerDownRitual.tsx
@@ -1525,6 +1525,7 @@ export function PowerDownRitual({ onComplete }: PowerDownRitualProps) {
                       </div>
                       <div className="flex-1 min-h-0 p-2">
                         <CalendarSplitView
+                          mode="schedule_tasks"
                           viewMode="day"
                           dateRange={tomorrowDateRange}
                           unscheduledItems={unscheduledTomorrowItems}
